6c7f833e412e56783bca63f6afb26e16dfcbfee5
oleksandrq <dybno.mg@gmail.com>
2020-11-01 18:15:32 +0200

    Add kotlin class

---
+++ src/main/kotlin/Address.kt
@@ -0,0 +1,10 @@
+//class Address(val town: String, val street: String, val number: Int ) {
+//
+//}
+//if (::str.isInitialized)
+class Address
+{
+    var str: String? = null
+    var num: Int? = null
+
+}
