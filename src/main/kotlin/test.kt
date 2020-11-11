40747be6abc008b0eb0538f7b881b93d3771db8b
oleksandrq <dybno.mg@gmail.com>
2020-11-11 22:38:38 +0200

    Add files for future

---
+++ src/main/kotlin/test.kt
@@ -0,0 +1,11 @@
+class test(num: Int, mac: String?) {
+    private val test1 = 0
+    private val str: String? = null
+
+    init {
+        var num = num
+        var mac = mac
+        num = test1
+        mac = str
+    }
+}
