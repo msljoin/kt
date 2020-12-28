7d302068761e8f5802d80ad7fbf413d67fe5c706
msljoin <dybno.mg@gmail.com>
2020-12-28 23:29:23 +0200

    Add new function, check MutableList

--- src/main/kotlin/collections/main.kt
+++ src/main/kotlin/collections/main.kt
@@ -1,5 +1,7 @@
 package collections
 
+import kotlin.random.Random
+
 fun List<String>.getShortWordsTo(shortWords: MutableList<String>, maxLength: Int) {
     this.filterTo(shortWords) { it.length <= maxLength }
     // throwing away the articles
@@ -8,8 +10,28 @@ fun List<String>.getShortWordsTo(shortWords: MutableList<String>, maxLength: Int
 }
 
 fun main() {
-    val words = "A long time ago in a galaxy far far away".split(" ")
-    val shortWords = mutableListOf<String>()
-    words.getShortWordsTo(shortWords, 3)
-    println(shortWords)
+//    val words = "A long time ago in a galaxy far far away".split(" ")
+//    val shortWords = mutableListOf<String>()
+//    words.getShortWordsTo(shortWords, 3)
+//    println(shortWords)
+    lists()
+}
+
+fun lists() {
+    val list: MutableList<Int> = mutableListOf()
+    var i: Int = 0
+    // fill list
+    while (i < 5) {
+        list.add(Random.nextInt(0, 10))
+        i++
+    }
+    println("list $list")
+    // sort val from s to h
+    list.sort()
+    println("list after sort() $list")
+    // remove first
+    list.removeAt(0)
+    // remove last
+    list.removeAt(list.size-1)
+    println("new $list")
 }
