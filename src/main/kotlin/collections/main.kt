e443ec54c3c83e3a0f9556ab18216833e07d062e
msljoin <dybno.mg@gmail.com>
2020-12-29 22:46:32 +0200

    add function like mapOf and Map

--- src/main/kotlin/collections/main.kt
+++ src/main/kotlin/collections/main.kt
@@ -14,7 +14,8 @@ fun main() {
 //    val shortWords = mutableListOf<String>()
 //    words.getShortWordsTo(shortWords, 3)
 //    println(shortWords)
-    lists()
+//    lists()
+    maps();
 }
 
 fun lists() {
@@ -34,4 +35,15 @@ fun lists() {
     // remove last
     list.removeAt(list.size-1)
     println("new $list")
+}
+
+fun maps() {
+    val a = mapOf(1 to "one", 2 to "two", 3 to "three")
+
+    val b: Map<String, Int> = mapOf("apple" to 10, "banana" to 5)
+
+    for (i in a)
+        println(i)
+
+    println(b)
 }
